37c82f23fdba14d169105c31d61ebeb31d3b010e
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2022-10-04 18:19:15 +0300

    Added fields validations

--- RazorPagesUI/Models/AddressModel.cs
+++ RazorPagesUI/Models/AddressModel.cs
@@ -1,11 +1,31 @@
-﻿namespace RazorPagesUI.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesUI.Models
 {
     public class AddressModel
     {
+        [BindProperty]
+        [Required]
+        [MinLength(8), MaxLength(30)]
         public string StreetAddress { get; set; }
+
+        [BindProperty]
+        [Required]
+        [MinLength(3), MaxLength(15)]
         public string City { get; set; }
+
+        [BindProperty]
+        [Required]
+        [MinLength(3), MaxLength(15)]
         public string State { get; set; }
+
+        [BindProperty]
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        [RegularExpression(@"(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)")]
         public string ZipCode { get; set; }
+
         public DateTime Created { get; set; } = DateTime.UtcNow;
     }
 }
--- RazorPagesUI/Models/UserModel.cs
+++ RazorPagesUI/Models/UserModel.cs
@@ -1,9 +1,20 @@
-﻿namespace RazorPagesUI.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesUI.Models
 {
-	public class UserModel
-	{
-		public string UserName { get; set; }
-		public int YearOfBirth { get; set; }
+    public class UserModel
+    {
+        [BindProperty]
+        [Required]
+        [MinLength(6), MaxLength(14)]
+        public string UserName { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Range(1900, 2004)]
+        public int YearOfBirth { get; set; }
+
         public DateTime Created { get; set; } = DateTime.UtcNow;
     }
 }
