9d84f36df7dd50dcb0c268c76d74092d407b4c79
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2022-10-15 01:19:58 +0300

    User model validation added

--- RazorPagesDemo.Models/User.cs
+++ RazorPagesDemo.Models/User.cs
@@ -6,14 +6,12 @@ namespace RazorPagesDemo.Models
     {
         public string Id { get; init; } = Guid.NewGuid().ToString();
 
-        //[BindProperty]
-        //[Required]
-        //[MinLength(3), MaxLength(14)]
+        [Required]
+        [MinLength(3), MaxLength(14)]
         public string Name { get; set; }
 
-        //[BindProperty]
-        //[Required]
-        //[Range(1900, 2004)]
+        [Required]
+        [Range(1900, 2004)]
         public int YearOfBirth { get; set; }
 
         public DateTime Created { get; init; } = DateTime.UtcNow;
